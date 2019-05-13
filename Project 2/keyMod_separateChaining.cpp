46944e7cbcec3fde4a1d87d12d79ef4fae58faa8
Blas Kojusner <bkojusner@ufl.edu>
2019-05-13 14:09:09 -0400

    Add files via upload

---
+++ Project 2/keyMod_separateChaining.cpp
@@ -0,0 +1,102 @@
+/// Key Mod Table Size
+/// Separate Chaining
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+//load factor method
+double loadFactor(int &num_elems, int num_buckets)
+{
+	return ((double)num_elems / (double)num_buckets);
+}
+
+//creating node
+class Node {
+	friend class HashTable;
+	Node* next;
+	int key;
+public:
+	Node(int k, Node* n) {
+		key = k;
+		next = n;
+	}
+};
+
+//declaring methods in HashTable class
+class HashTable {
+public:
+	int size;
+	vector<Node*>* buckets;
+
+	//HashTable size
+	HashTable(int s) {
+		size = s;
+		buckets = new vector<Node*>(size);
+	}
+
+	//insert method
+	void insert(int key) {
+		if (buckets->at(hashFunction(key)) == NULL) {
+			buckets->at(hashFunction(key)) = new Node(key, NULL);
+			return;
+		}
+
+		while (buckets->at(hashFunction(key)) != NULL) {
+			if (buckets->at(hashFunction(key))->next == NULL) {
+				buckets->at(hashFunction(key))->next = new Node(key, NULL);
+				break;
+			}
+			buckets->at(hashFunction(key)) = buckets->at(hashFunction(key))->next;
+		}
+	}
+
+	//hash function key mod size
+	int hashFunction(int num) {
+		return num % size;
+	}
+
+	//print method
+	void print(int start) {
+		int lineCount = 0;
+		int collision = 0;
+		int count = 0;
+
+		for (int i = start; i < buckets->size(); i++) {
+			while (buckets->at(i) != NULL) {
+				buckets->at(i) = buckets->at(i)->next;
+				count++;
+
+				//count collisions
+				if (buckets->at(i) != NULL)
+					collision++;
+			}
+
+			//get load factor
+			double load = loadFactor(count, buckets->size());
+
+			cout << "Collisions: " << collision << endl;
+			cout << "Load Factor: " << load << endl;
+		}
+	}
+};
+
+int main(int argc, const char* argv[]) {
+	int size = 7; //Adjustable amount of buckets
+	int items = size * size; //total number of items
+	int hashtableSize = size * items;
+	HashTable* hash = new HashTable(size); //hashtable pointer
+
+	for (int i = 0; i < items; i++) {
+		//key generated and inserted
+		int key = 0 + (rand() * (int)((hashtableSize * 3) - 0) / RAND_MAX);
+		hash->insert(key);
+	}
+
+	//print details
+	hash->print(0);
+
+	return 0;
+}
---
+++ Project 2/midSquare_openAddressing.cpp
@@ -0,0 +1,70 @@
+///Mid Square
+///Open Addressing
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+
+using namespace std;
+
+//mid square hashing function
+int hashFunction(int key, int sizeTable)
+{
+	key = key*key;
+	key = key / 100;
+	key = key % sizeTable;
+	return key;
+}
+
+//load factor function
+double loadFactor(int items, int buckets)
+{
+	return ((double)items / (double)buckets);
+}
+
+int main() {
+	int size = 7; //number of items in bucket (Adjustable)
+	int buckets = 7; //number of buckets in the hashtable (Adjustable)
+	int hashtable[7][7]; //(Adjustable)
+	int hashtableSize = size * buckets; //size of the hashtable
+
+	//fill out hashtable with temporary place holders
+	for (int i = 0; i < buckets; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			hashtable[i][j] = -2;
+		}
+	}
+
+	//variables to track counting
+	int items = 0;
+	int collisions = 0;
+	double loadfactor = 0.0;
+
+	for (int n = 0; n < hashtableSize; n++)
+	{
+		//key generation and hashing
+		int key = 0 + (rand() * (int)((3 * hashtableSize) - 0) / RAND_MAX);
+		int hidx = hashFunction(key, buckets);
+
+		//checking collision and load factor
+		for (int m = 0; m < size; m++)
+		{
+			if (hashtable[hidx][m] == -2)
+			{
+				//put key in bucket
+				hashtable[hidx][m] = key;
+				items++;
+				cout << "Collisions: " << collisions << endl;
+				loadfactor = loadFactor(items, buckets);
+				cout << "Load Factor: " << loadfactor << endl;
+				break;
+			}
+			else
+			{
+				collisions++;
+			}
+		}
+	}
+	return 0;
+}
